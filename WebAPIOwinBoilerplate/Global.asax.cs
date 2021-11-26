ea5a79350c223212941e292807740c08db9c042f
Tony Pires <ampires9@gmail.com>
2021-11-26 18:13:59 +0000

    Amended comments in Global.asax.cs

--- WebAPIOwinBoilerplate/Global.asax.cs
+++ WebAPIOwinBoilerplate/Global.asax.cs
@@ -13,11 +13,11 @@ namespace WebAPIOwinBoilerplate
         {
             // This is the default entry point to any new Web API project.
 
-            // This entry point can be replaced by the OWIN Startup.cs class.  The Configuration method (which is required)
-            // is where WebApi configuration is usually stored (i.e. WebApiConfig.Register).  
+            // This entry point can be replaced by the OWIN Startup.cs class.  The Configuration method (which is required by the Microsoft.Owin.Host.SystemWeb package)
+            // is where WebApi configuration is stored (i.e. WebApiConfig.Register).
 
             // This Application_Start() method has been left here for the purposes of demonstrating the default entry point for Web Api application which 
-            // can be replaced with Startup.cs.
+            // can be replaced or co-exist with Startup.cs.  
 
             //GlobalConfiguration.Configure(WebApiConfig.Register);
         }
